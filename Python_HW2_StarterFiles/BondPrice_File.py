10a38bbd5794a82b921239056aa604d1bdfaa9e6
js6913-creator <js6913@columbia.edu>
2026-02-26 19:48:05 -0500

    Complete getBondPrice

--- Python_HW2_StarterFiles/BondPrice_File.py
+++ Python_HW2_StarterFiles/BondPrice_File.py
@@ -1,8 +1,15 @@
+
 import numpy as np
 
-def getBondPrice(y, face, couponRate, m, ppy=1):
-    if ppy == 1:
-        x = 2170604
-    if ppy == 2:
-        x = 2171686
-    return(x)
+def getBondPrice(y, face, couponRate, m, ppv=1):
+    
+    n = m * ppv
+    C = face * couponRate / ppv
+    
+    t = np.arange(1, n+1)
+    discount = 1 / (1 + y/ppv)**t
+    
+    couponPV = np.sum(C * discount)
+    facePV = face / (1 + y/ppv)**n
+    
+    return couponPV + facePV
